1fd270e26d5aaadd0a20197dae1607388103aa37
Callum Saxon <78568087+callum-saxon@users.noreply.github.com>
2023-11-17 09:59:51 +0000

    Addition of multiple images as a ref

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import cv2
+import os
 from deepface import DeepFace
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -8,31 +9,37 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 face_match = False
-ref_img = cv2.imread("ref.jpg")
 
-try:
-    ref_img = cv2.imread("ref.jpg")
-    if ref_img is None:
-        raise ValueError("Image PATH may be inccorrect/Image may be invalid")
-except Exception as e:
-    print(f"Error loading reference image: {e}")
-    exit()
+ref_images_folder = "ref_imgs"
+ref_image_paths = [os.path.join(ref_images_folder, f) for f in os.listdir(ref_images_folder) if f.endswith(('.jpg', '.png'))]
+ref_images = []
+
+for path in ref_image_paths:
+    try:
+        img = cv2.imread(path)
+        if img is None:
+            raise ValueError(f"Image PATH {path} may be incorrect/Image may be invalid")
+        ref_images.append(img)
+    except Exception as e:
+        print(f"Error loading reference image {path}: {e}")
+        exit()
 
 def check_face(frame):
     global face_match
     try:
-        result = DeepFace.verify(frame, ref_img, enforce_detection=False)
-        if result['verified']:
-            face_match = True
-        else:
-            face_match = False
+        for ref_img in ref_images:
+            result = DeepFace.verify(frame, ref_img, enforce_detection=False)
+            if result['verified']:
+                face_match = True
+                break
+            else:
+                face_match = False
 
     except ValueError:
         face_match = False
 
 while True:
     ret, frame = cap.read()
-
     if ret:
         frame = cv2.flip(frame, 1)
         if counter % 30 == 0:
@@ -44,6 +51,10 @@ while True:
         counter += 1
 
         if face_match:
+            x, y, w, h = 20, 20, 600, 440
+            roi = frame[y:y+h, x:x+w]
+            blurred_roi = cv2.GaussianBlur(roi, (99, 99), 0)
+            frame[y:y+h, x:x+w] = blurred_roi
             cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
         else:
             cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
